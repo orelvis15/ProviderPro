79017d88843e0eaa032ec00a54167e56130061fd
Orelvis Lago <orelvis15@gmail.com>
2018-02-19 18:49:58 -0300

    cambiando la forma de enlazar el modelo de usuario

--- api/config/passport.js
+++ api/config/passport.js
@@ -2,7 +2,8 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mongoose = require('mongoose');
 //var User = mongoose.model('User');
-var User = require('../models/User');
+//var User = require('../models/User');
+var User = mongoose.model('User');
 
 passport.use(new LocalStrategy({
         usernameField: 'email'
