3a10cf7fd5a283514281f1b000a22947d68b4c12
Orelvis Lago <orelvis15@gmail.com>
2018-02-19 18:54:33 -0300

    configurando ruta de modelo de usuario en passport

--- api/config/passport.js
+++ api/config/passport.js
@@ -2,8 +2,7 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mongoose = require('mongoose');
 //var User = mongoose.model('User');
-//var User = require('../models/User');
-var User = mongoose.model('User');
+var User = require('./../models/User');
 
 passport.use(new LocalStrategy({
         usernameField: 'email'
